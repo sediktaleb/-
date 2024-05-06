f55c9bacd48e18d8ba40a931b8784f9de02d1467
sediktaleb <167780734+sediktaleb@users.noreply.github.com>
2024-05-06 18:45:31 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,44 @@
+function Click(person){
+var name = person.id;
+
+    document.querySelector(".nsba").style.display='block';
+    document.querySelector(".panel").style.display='none';
+    var nsba = document.getElementById("s7a");
+    var blyma = Math.floor(Math.random() * (100-0))+0;
+
+    if(name == "Aryam"){
+        var aryam = Math.floor(Math.random() * (100-90))+90;
+        nsba.innerHTML=aryam + "%";
+    }
+    else if(name == "Douaa"){
+        nsba.innerHTML="I Cant Count Maybe Inf %";
+
+
+    }
+    else if(name == "Sirine"){
+        nsba.innerHTML="She is Start Cry";
+
+
+    }
+    else if(name == "Sedik"){
+        nsba.innerHTML="0 %";
+
+
+    } else if(name == "Fwaz"){
+        nsba.innerHTML="Eror 404";
+
+
+    }
+    else if(name == "Iyad"){
+        var aryam = Math.floor(Math.random() * (200-100))+100;
+        nsba.innerHTML=aryam+"%";
+
+
+    }
+    else{
+        nsba.innerHTML=blyma + "%";
+
+    }
+
+}
+document.querySelector(".nsba").style.display='none';
