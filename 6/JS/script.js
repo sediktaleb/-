192809da2b916e66f7ab5700d49061c133a12a39
sediktaleb <167780734+sediktaleb@users.noreply.github.com>
2024-05-12 12:24:32 +0100

    Add files via upload

---
+++ 6/JS/script.js
@@ -0,0 +1,62 @@
+var names = document.getElementById("names")
+names.style.display="none";
+var click = 1;
+function Names(){
+    if (click == 1){
+     
+        names.style.display="block";
+    click = 0
+    }else{
+        names.style.display="none";
+    click = 1;
+    }
+}
+
+function ServiesClick(value){
+    var id = value.id;
+        if(id == "Html"){
+            window.scroll(0,2200);
+        }else if(id == "Css"){
+            window.scroll(0,2200);
+
+        }else if(id == "JS"){
+            window.scroll(0,2200);
+
+        }
+};
+
+function order(){
+    var username = document.getElementById("uname");
+    var email = document.getElementById("email");
+    var order = document.getElementById("order");
+        if(username.value && email.value && order.value){
+            //alert("Name ("+username.value+"),"+"Email ("+email.value+"),"+"Order ("+order.value+").");
+            sendEmail();
+        }else{
+            alert("ادخل المعلومات اولا")       
+         }
+
+}
+
+function sendEmail(){
+    var parmas = {
+        name : document.getElementById("uname").value,
+        email : document.getElementById("email").value,
+        order : document.getElementById("order").value,
+
+    };
+    const serviceID = "service_9jj3m2r";
+    const templateID = "template_d458ss4";
+    
+    emailjs.send(serviceID,templateID,parmas).then(function(red){
+        var name = document.getElementById("uname").value;
+        var email = document.getElementById("email").value;
+        var order = document.getElementById("order").value;
+        alert(name+" , "+email+" , "+order,+" ;");
+        alert(" ان كانت المعلومات صحيحة سوف يتم التواصل معك");
+
+    }
+            
+      
+    
+    )};
